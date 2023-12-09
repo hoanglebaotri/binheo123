89b5e24f129d7803471cf5b1bdee35fa59e74211
hoanglebaotri <91924806+hoanglebaotri@users.noreply.github.com>
2023-12-09 16:25:54 +0700

    vc

---
+++ script.js
@@ -0,0 +1,23 @@
+// let box = document.querySelector('.box');
+// window.onmousemove = function(e){
+//     let x = e.clientX/3;
+//     box.style.transform = "perspective(1000px) rotateY(" + x + "deg)";
+// }
+// alert(" con cặc");
+
+// let prev = document.querySelector('.prev');
+// let next = document.querySelector('.next');
+// let box = document.querySelector('.box');
+
+// let dagrees = 0 ;
+
+// prev.addEventListener('click', function(){
+//     dagrees +=45;
+//     box.style='transform: perspective(1000px) rotateY(${dagrees}deg)';
+// })
+// next.addEventListener('click', function(){
+//     dagrees -=45;
+//     box.style= 'transform: perspective(1000px) rotateY(${dagrees}deg)';
+// })
+
+
